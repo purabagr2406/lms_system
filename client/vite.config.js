8ae7f93b105d523f750b9b1cd716655023ba764f
Rishabh Singh <rishabh251204@gmail.com>
2025-01-23 11:20:58 +0530

    Adding Authentication to Login and Signup - I

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -10,12 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 5ab92422f06e32926d7bbfaf2cc78bfa1223b283
 })
