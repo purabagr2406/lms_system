77086497faadc73d4fb894af16642974e458be34
Rishabh Singh <rishabh251204@gmail.com>
2025-01-29 12:03:01 +0530

    Built the pages for HeroSection, Courses

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,36 @@
+import { createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Login from './pages/Login'
+import HeroSection from './pages/student/HeroSection'
+import MainLayout from './layout/MainLayout'
+import { RouterProvider } from 'react-router'
+import Courses from './pages/student/Courses'
 
+const appRouter = createBrowserRouter([
+	{
+		path:"/",
+		element:<MainLayout/>,
+		children:[
+			{
+				path:"/",
+				element:
+				<>
+					<HeroSection />
+					<Courses />
+				</>
+			},
+			{
+				path:"login",
+				element:<Login/>
+			}
+		]
+	}
+])
 function App() {
 	return (
 		<main>
-			<Navbar />
-			<Login>hello</Login>
+			<RouterProvider router={appRouter}/>
 		</main>
 	)
 }
