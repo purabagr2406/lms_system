c355e6375eeaefd1063d5e62c571a514ed3bc99b
Rishabh Singh <rishabh251204@gmail.com>
2025-06-30 03:06:21 +0530

    Deployment 1.5

--- client/src/pages/admin/course/EditCourse.jsx
+++ client/src/pages/admin/course/EditCourse.jsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
 import { Link } from 'react-router-dom'
-import CourseTab from './Coursetab'
+import CourseTab from './CourseTab'
 
 const EditCourse = () => {
 	return (
